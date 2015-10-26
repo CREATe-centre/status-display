ed3eb1ecf1eaee3c87bfb9e13c6cc2599743c446
Dominic Price <dominic.price@nottingham.ac.uk>
2015-10-26 22:29:42 +0000

    Added lib folder to tests with ignore for 3rd party code

--- tests.sh
+++ tests.sh
@@ -2,5 +2,6 @@
 
 find src \( -iname '*.php' \) -print0 | xargs -n1 -0 php -l &&
 find tests \( -iname '*.php' \) -print0 | xargs -n1 -0 php -l &&
-phpcs -psvn --standard=WordPress --ignore=lib src &&
+phpcs -psvn --standard=WordPress --ignore=lib/codebird.php \
+    --ignore=lib/uuid.php src &&
 phpunit -c phpunit.xml
